cad6e11dbb29331039059f1c22060845cc408bb6
Daniel Hernandez <00209317@uca.edu.sv>
2018-09-28 14:39:25 -0600

    add file 1

---
+++ ejercicio1.js
@@ -0,0 +1,13 @@
+var array = [20, 20, 20, 50, 60, 70];
+
+function recurrencia(n, lista) {
+    let cont = 0;
+    for(let i = 0; i < lista.length; i++){
+        if(lista[i] == n){
+            cont++;
+        }
+    }
+    return cont;
+}
+
+recurrencia(20, array);
